d835ccd4461c219986ff1539553c0a602c6a47f8
n43ee7 <nabeel.nayyar6113@outlook.com>
2021-02-05 22:40:56 -0600

    Update RSSD.py

--- NODE 1/RSSD.py
+++ NODE 1/RSSD.py
@@ -1,12 +1,9 @@
+#!/usr/bin/env python3
 import serial
-import os
-
-def serialSearch():
-    os.
-with serial.Serial('/dev/ttyACM0', 9600, timeout=10) as ser:
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
     while True:
-        led_on = input('Do you want the LED on? ')[0]
-        if led_on in 'yY':
-            ser.write(bytes('YES\n','utf-8'))
-        if led_on in 'Nn':
-            ser.write(bytes('NO\n','utf-8'))
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
